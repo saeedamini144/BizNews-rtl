dfc00ed2bdb3719e1ba5463be239ebcaa7e7efe5
SaeedAmini <saaed.amini@gmail.com>
2024-12-16 09:27:10 +0330

    delet the default meta tag

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    <meta content="Free HTML Templates" name="keywords" />
-    <meta content="Free HTML Templates" name="description" />
     <?php
     wp_head(); //call The header
 
