52860cce59d3fec269f569d6e35af45c25c54e51
SaeedAmini <saaed.amini@gmail.com>
2024-12-22 15:24:18 +0330

    corect the date color

--- template-parts/postWithoutImage-card.php
+++ template-parts/postWithoutImage-card.php
@@ -31,7 +31,7 @@ if (!empty($choose_category)) {
             <div class="mb-3">
                 <div class="mb-2">
                     <a class="badge badge-primary font-weight-semi-bold p-1 ml-2" href="<?php echo get_category_link(get_the_category()[0]->term_id) ?>"><?php echo get_the_category()[0]->name ?></a>
-                    <span><small><?php echo display_jalali_date('Y/m/d', get_the_time('U')); ?></small></span>
+                    <span class="text-light"><small><?php echo display_jalali_date('Y/m/d', get_the_time('U')); ?></small></span>
                 </div>
                 <a class="small text-body font-weight-medium" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
             </div>
